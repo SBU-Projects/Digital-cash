43f7e0ba52eba0e72f5c6cf1fe14f085c5fbd324
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-03 12:01:13 +0330

    bank

--- functions/bank.py
+++ functions/bank.py
@@ -2,3 +2,8 @@ class Bank:
     def __init__(self, name, public_data):
         print("The {} bank constructor is called!".format(name))
         self.public_data = public_data
+        self.x = 0
+        for n in name:
+            self.x += ord(n)
+
+        print(self.x)
