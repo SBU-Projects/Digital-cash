0ae56e1fc1e03ba219ad3edd2469cbdfc6187218
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-03 11:14:00 +0330

    generate_prime_pair

--- functions/initialization.py
+++ functions/initialization.py
@@ -1,4 +1,17 @@
+from sympy import isprime, nextprime, primitive_root
+
 class Initialization:
     def __init__(self, large_prime_number):
         print("Initializing with large prime {}".format(large_prime_number))
         self.p = large_prime_number
+
+    def generate_prime_pair(self):
+        curr_p = self.p
+        while True:
+            curr_p = nextprime(curr_p)  # Find the next prime number
+            q = (curr_p - 1) // 2  # Calculate q
+            if isprime(curr_p):  # Check if q is also prime
+                return q
+    def set_public_arguments(self):
+        self.q = self.generate_prime_pair()
+
--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-from sympy import isprime, nextprime, primitive_root
 
-def generate_prime_pair():
-    p = 28871271685163  # Start with the smallest prime number
-    while True:
-        p = nextprime(p)  # Find the next prime number
-        q = (p - 1) // 2  # Calculate q
-        if isprime(q):  # Check if q is also prime
-            return p, q
 
 
 
