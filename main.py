a4c18fccc4763668ae3305b9fa1cb67105458242
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-03 12:07:33 +0330

    bank get_public_idetifiers

--- main.py
+++ main.py
@@ -2,8 +2,4 @@ from functions.initialization import Initialization
 from functions.bank import Bank
 
 init1 = Initialization(28871271685163)
-print(init1.get_public_arguments())
-print(init1.get_private_arguments())
-
 bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())
-print(bank1.get_public_idetifiers())
