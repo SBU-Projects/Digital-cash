010948866b76127af467705b5b775d5f2b456b31
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-03 11:03:05 +0330

    generate_prime_pair

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from sympy import isprime, nextprime
 
 def generate_prime_pair():
-    p = 2  # Start with the smallest prime number
+    p = 28871271685163  # Start with the smallest prime number
     while True:
         p = nextprime(p)  # Find the next prime number
         q = (p - 1) // 2  # Calculate q
@@ -10,4 +10,4 @@ def generate_prime_pair():
 
 # Generate the prime pair
 prime_p, prime_q = generate_prime_pair()
-prime_p, prime_q
+print(prime_p, prime_q)
