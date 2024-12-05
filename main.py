661e565215ab9e079f5f04a8d43ee0e2b27c5c8c
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-05 13:34:03 +0330

    comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from functions.initialization import Initialization
+"""from functions.initialization import Initialization
 from functions.bank import Bank
 
 init1 = Initialization(28871271685163)
-bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())
+bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())"""
