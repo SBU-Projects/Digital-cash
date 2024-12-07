738cf93118c0dbe1d1f412748db3446835647108
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-07 23:35:45 +0330

    spender

--- functions/spender.py
+++ functions/spender.py
@@ -1,9 +1,13 @@
 class Spender:
 
     def __init__(self, name, public_data):
-        print("The {} bank constructor is called!".format(name))
+        print("{} constructor is called as a spender!".format(name))
         self.public_data = public_data
         self.name = name
-        self.x = 0
+        self.u = 0
         for n in name:
             self.u += ord(n)
+
+        g1 = public_data['g1']
+        p = public_data['p']
+        self.I = pow(g1, self.u, p)
