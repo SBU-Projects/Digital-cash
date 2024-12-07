6f432edf7cf3d8387e9a34640e6ce61bccefe5e0
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-07 23:24:53 +0330

    comment test

--- main.py
+++ main.py
@@ -3,28 +3,3 @@ from functions.bank import Bank
 
 init1 = Initialization(28871271685163)
 bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())"""
-
-
-def generate_partitions(m, n, current_partition=[], start=1):
-    # Base case: If we need 1 part, the only valid partition is the remaining value
-    if n == 1:
-        if m >= start:
-            return [current_partition + [m]]
-        else:
-            return []
-
-    # Recursive case: Generate partitions
-    partitions = []
-    for first in range(start, m - n + 2):  # Ensure non-decreasing order by using `start`
-        print(first)
-        print("P({}) -> {}".format((m - first, n - 1, current_partition + [first], first), generate_partitions(m - first, n - 1, current_partition + [first], first)))
-        partitions += generate_partitions(m - first, n - 1, current_partition + [first], first)
-    return partitions
-
-
-# Example usage
-m = 5
-n = 3
-partitions = generate_partitions(m, n)
-print(f"Partitions of {m} into {n} parts:", partitions)
-
