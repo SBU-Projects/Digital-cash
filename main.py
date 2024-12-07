07e01abb194f3f71da07bc7e34752d9aee7d57c6
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-07 23:39:09 +0330

    spender

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-"""from functions.initialization import Initialization
+from functions.initialization import Initialization
 from functions.bank import Bank
+from functions.spender import Spender
 
 init1 = Initialization(28871271685163)
-bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())"""
+bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())
+spender1 = Spender("Sajjad", init1.get_public_arguments())
