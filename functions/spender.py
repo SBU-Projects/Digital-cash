a35421025b3950169b8f309c6e2c73c808de8b0d
Sajjad <sajjadranjbaryazdi@gmail.com>
2024-12-08 15:16:19 +0330

    creating_coin

--- functions/spender.py
+++ functions/spender.py
@@ -1,3 +1,4 @@
+import random
 class Spender:
 
     def __init__(self, name, public_data):
@@ -28,5 +29,33 @@ class Spender:
     def connect_with_bank(self, bank):
         print("{} conected to {} bank".format(self.name, bank.get_public_idetifiers()['name']))
         self.z_prime = bank.connect_with_spender(self.get_public_idetifiers())
+
+
+    def creating_coin_spender(self, order, input_tuple):
+        if self.z_prime > 0:
+            random_variables = {
+                "s": random.randint(1000, 2000),
+                "x1": random.randint(1000, 2000),
+                "x2":  random.randint(1000, 2000),
+                "alpha1": random.randint(1000, 2000),
+                "alpha2": random.randint(1000, 2000)
+            }
+
+            g = self.get_public_arguments_by_index("g")
+            g1 = self.get_public_arguments_by_index("g1")
+            g2 = self.get_public_arguments_by_index("g2")
+            p = self.get_public_arguments_by_index("p")
+            A = pow((self.I*g2), random_variables['s'], p)
+            data = {
+                "A": A,
+                "B": (pow(g1, random_variables['x1']) * pow(g2, random_variables['x2']))%p,
+                "z": pow(self.z_prime, random_variables['s'], p),
+                "a": (pow(input_tuple["gw"], random_variables['alpha1'])*pow(g, random_variables['alpha2']))%p,
+                "b": (pow(input_tuple["betha"], random_variables['s']*random_variables['alpha1']) * pow(A, random_variables['alpha2'])) % p
+            }
+
+            return data
+        else:
+            raise ValueError("The sender does not connected to the bank!")
 
 
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from functions.spender import Spender
 init1 = Initialization(28871271685163)
 bank1 = Bank("Dr.Ziba Eslami", init1.get_public_arguments())
 spender1 = Spender("Sajjad", init1.get_public_arguments())
-
-print(bank1.creating_coin_bank("gw-betha", [1]))
+spender1.connect_with_bank(bank1)
+print(spender1.creating_coin_spender(1, {"gw": 1, "betha": 1}))
 """
 print(bank1.get_conected_spenders())
 spender1.connect_with_bank(bank1)
